7dc9ac74b37633704dab98d29b3a69c450ed5622
Greg Tucker-Kellogg <dbsgtk@gmail.com>
2019-06-15 12:42:48 +0800

    Updated from a long time ago

--- init.el
+++ init.el
@@ -1,30 +1,11 @@
-;;; init.el --- Where all the magic begins
-;;
-;; Part of the Emacs Starter Kit
-;;
-;; This is the first thing to get loaded.
-;;
-
-;; load Org-mode from source when the ORG_HOME environment variable is
-;;set
-
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+
 (package-initialize)
 
-(setq load-path (delq "/usr/share/emacs/site-lisp/dictionaries-common" load-path))
-
-;;; Code:
-(when (getenv "ORG_HOME")
-  (let ((org-lisp-dir (expand-file-name "lisp" (getenv "ORG_HOME"))))
-    (when (file-directory-p org-lisp-dir)
-      (add-to-list 'load-path org-lisp-dir)
-      (require 'org))))
-
-;; load the starter kit from the `after-init-hook' so all packages are loaded
 (add-hook 'after-init-hook
  `(lambda ()
     ;; remember this directory
@@ -35,7 +16,6 @@
                   (file-directory-p (expand-file-name "lisp"
                                                       (getenv "ORG_HOME"))))
        '(require 'org))
-    ;; load up the starter kit
     (org-babel-load-file (expand-file-name "starter-kit.org" starter-kit-dir))))
 
   (defun expand-directory-name (d)
